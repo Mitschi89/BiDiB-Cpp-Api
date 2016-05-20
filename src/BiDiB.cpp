9d859c043a1d5c8b0d1297f14fa2bc63f546a3cf
Michael Scharfenberg <michael.scharfenberg@tu-ilmenau.de>
2016-05-20 15:41:27 +0200

    Correct Format and Indentions

--- src/BiDiB.cpp
+++ src/BiDiB.cpp
@@ -21,101 +21,146 @@ using namespace std;
 
 int locCount = 0;
 int speed = 50;
-Segment::segmentID criticalState[MAXNUMBEROFSEGEMENTSWITHLOC] = {Segment::DEFAULT};
+Segment::segmentID criticalState[MAXNUMBEROFSEGEMENTSWITHLOC] = {
+		Segment::DEFAULT };
 
-void handlePredictions(BidibApi *api){
+void handlePredictions(BidibApi *api) {
 
-	if(locCount != api->getNumberOfLocs()){
+	if (locCount != api->getNumberOfLocs()) {
 		locCount = api->getNumberOfLocs();
-		for(int locID = 0; locID < locCount; locID++){
+		for (int locID = 0; locID < locCount; locID++) {
 			api->setLocSpeed(locID, speed, true);
 		}
 	}
 
-	for(int locID = 0; locID < locCount; locID++){
-		if(api->getLocSpeed(locID) != speed){
+	for (int locID = 0; locID < locCount; locID++) {
+		if (api->getLocSpeed(locID) != speed) {
 			api->setLocSpeed(locID, speed, true);
 		}
 	}
 
-	for(int locID = 0; locID < locCount; locID++){
+	for (int locID = 0; locID < locCount; locID++) {
 
-		if(api->getCurrentState(locID) != criticalState[locID]){
+		if (api->getCurrentState(locID) != criticalState[locID]) {
 			criticalState[locID] = Segment::DEFAULT;
 		}
 
-		if((api->getCurrentState(locID) == Segment::A1) || (api->getCurrentState(locID) == Segment::A2)){
-			if(!(api->isLocOnPosition(locID, Segment::TA1)) && !(api->isLocOnPosition(locID, Segment::TA2))){
-				if((criticalState[locID] != Segment::A1) && (criticalState[locID] != Segment::A2)){
-					if(api->getLocLogicalDirection(locID) == false){
-						api->setLocSpeed(locID, speed, !api->getLocDirection(locID));
+		if ((api->getCurrentState(locID) == Segment::A1)
+				|| (api->getCurrentState(locID) == Segment::A2)) {
+			if (!(api->isLocOnPosition(locID, Segment::TA1))
+					&& !(api->isLocOnPosition(locID, Segment::TA2))) {
+				if ((criticalState[locID] != Segment::A1)
+						&& (criticalState[locID] != Segment::A2)) {
+					if (api->getLocLogicalDirection(locID) == false) {
+						api->setLocSpeed(locID, speed,
+								!api->getLocDirection(locID));
 						criticalState[locID] = api->getCurrentState(locID);
 					}
 				}
 			}
 		}
 
-		for(int nextLocID = locID+1; nextLocID < locCount; nextLocID++){
+		for (int nextLocID = locID + 1; nextLocID < locCount; nextLocID++) {
 
-			if(api->getCurrentState(nextLocID) != criticalState[nextLocID]){
+			if (api->getCurrentState(nextLocID) != criticalState[nextLocID]) {
 				criticalState[nextLocID] = Segment::DEFAULT;
 			}
 
-			if(api->getPredictedNextState(locID) == api->getPredictedNextState(nextLocID)){
-				if((api->getCurrentState(locID) != criticalState[locID]) &&
-						(api->getCurrentState(nextLocID) != criticalState[nextLocID])){
+			if (api->getPredictedNextState(locID)
+					== api->getPredictedNextState(nextLocID)) {
+				if ((api->getCurrentState(locID) != criticalState[locID])
+						&& (api->getCurrentState(nextLocID)
+								!= criticalState[nextLocID])) {
 
-					api->setLocSpeed(locID, speed, !api->getLocDirection(locID));
-					api->setLocSpeed(nextLocID, speed, !api->getLocDirection(nextLocID));
+					api->setLocSpeed(locID, speed,
+							!api->getLocDirection(locID));
+					api->setLocSpeed(nextLocID, speed,
+							!api->getLocDirection(nextLocID));
 					criticalState[locID] = api->getCurrentState(locID);
 					criticalState[nextLocID] = api->getCurrentState(nextLocID);
-					printf("Equal Predicted State: loc: %d and loc: %d at position: %s\n" ,
-							locID, nextLocID, Segment::convertSegmentName(api->getPredictedNextState(locID)));fflush(stdout);
+					printf(
+							"Equal Predicted State: loc: %d and loc: %d at position: %s\n",
+							locID, nextLocID,
+							Segment::convertSegmentName(
+									api->getPredictedNextState(locID)));
+					fflush(stdout);
 				}
 			}
-			if(api->getPredictedNextState(locID) == api->getCurrentState(nextLocID)){
-				if(api->getLocLogicalDirection(locID) == api->getLocLogicalDirection(nextLocID)){
-					api->setLocSpeed(locID, speed / 2, api->getLocDirection(locID));
-				}else{
-					if((api->getCurrentState(locID) != criticalState[locID]) &&
-							(api->getCurrentState(nextLocID) != criticalState[nextLocID])){
+			if (api->getPredictedNextState(locID)
+					== api->getCurrentState(nextLocID)) {
+				if (api->getLocLogicalDirection(locID)
+						== api->getLocLogicalDirection(nextLocID)) {
+					api->setLocSpeed(locID, speed / 2,
+							api->getLocDirection(locID));
+				} else {
+					if ((api->getCurrentState(locID) != criticalState[locID])
+							&& (api->getCurrentState(nextLocID)
+									!= criticalState[nextLocID])) {
 
-						api->setLocSpeed(locID, speed, !api->getLocDirection(locID));
-						api->setLocSpeed(nextLocID, speed, !api->getLocDirection(nextLocID));
+						api->setLocSpeed(locID, speed,
+								!api->getLocDirection(locID));
+						api->setLocSpeed(nextLocID, speed,
+								!api->getLocDirection(nextLocID));
 						criticalState[locID] = api->getCurrentState(locID);
-						criticalState[nextLocID] = api->getCurrentState(nextLocID);
-						printf("Predicted State is current State: loc: %d and loc: %d at position: %s\n" ,
-								locID, nextLocID, Segment::convertSegmentName(api->getPredictedNextState(locID)));fflush(stdout);
+						criticalState[nextLocID] = api->getCurrentState(
+								nextLocID);
+						printf(
+								"Predicted State is current State: loc: %d and loc: %d at position: %s\n",
+								locID, nextLocID,
+								Segment::convertSegmentName(
+										api->getPredictedNextState(locID)));
+						fflush(stdout);
 					}
 				}
 			}
-			if(api->getCurrentState(locID) == api->getPredictedNextState(nextLocID)){
-				if(api->getLocLogicalDirection(locID) == api->getLocLogicalDirection(nextLocID)){
-					api->setLocSpeed(nextLocID, speed / 2, api->getLocDirection(nextLocID));
-				}else{
-					if((api->getCurrentState(locID) != criticalState[locID]) &&
-							(api->getCurrentState(nextLocID) != criticalState[nextLocID])){
+			if (api->getCurrentState(locID)
+					== api->getPredictedNextState(nextLocID)) {
+				if (api->getLocLogicalDirection(locID)
+						== api->getLocLogicalDirection(nextLocID)) {
+					api->setLocSpeed(nextLocID, speed / 2,
+							api->getLocDirection(nextLocID));
+				} else {
+					if ((api->getCurrentState(locID) != criticalState[locID])
+							&& (api->getCurrentState(nextLocID)
+									!= criticalState[nextLocID])) {
 
-						api->setLocSpeed(locID, speed, !api->getLocDirection(locID));
-						api->setLocSpeed(nextLocID, speed, !api->getLocDirection(nextLocID));
+						api->setLocSpeed(locID, speed,
+								!api->getLocDirection(locID));
+						api->setLocSpeed(nextLocID, speed,
+								!api->getLocDirection(nextLocID));
 						criticalState[locID] = api->getCurrentState(locID);
-						criticalState[nextLocID] = api->getCurrentState(nextLocID);
-						printf("current State is Predicted State: loc: %d and loc: %d at position: %s\n" ,
-								locID, nextLocID, Segment::convertSegmentName(api->getCurrentState(locID)));fflush(stdout);
+						criticalState[nextLocID] = api->getCurrentState(
+								nextLocID);
+						printf(
+								"current State is Predicted State: loc: %d and loc: %d at position: %s\n",
+								locID, nextLocID,
+								Segment::convertSegmentName(
+										api->getCurrentState(locID)));
+						fflush(stdout);
 					}
 				}
 			}
-			if(api->getCurrentState(locID) == api->getCurrentState(nextLocID)){
-				if(api->getLocLogicalDirection(locID) != api->getLocLogicalDirection(nextLocID)){
-					if((api->getCurrentState(locID) != criticalState[locID]) &&
-							(api->getCurrentState(nextLocID) != criticalState[nextLocID])){
+			if (api->getCurrentState(locID)
+					== api->getCurrentState(nextLocID)) {
+				if (api->getLocLogicalDirection(locID)
+						!= api->getLocLogicalDirection(nextLocID)) {
+					if ((api->getCurrentState(locID) != criticalState[locID])
+							&& (api->getCurrentState(nextLocID)
+									!= criticalState[nextLocID])) {
 
-						api->setLocSpeed(locID, speed, !api->getLocDirection(locID));
-						api->setLocSpeed(nextLocID, speed, !api->getLocDirection(nextLocID));
+						api->setLocSpeed(locID, speed,
+								!api->getLocDirection(locID));
+						api->setLocSpeed(nextLocID, speed,
+								!api->getLocDirection(nextLocID));
 						criticalState[locID] = api->getCurrentState(locID);
-						criticalState[nextLocID] = api->getCurrentState(nextLocID);
-						printf("current state is current state: loc: %d and loc: %d at position: %s\n" ,
-								locID, nextLocID, Segment::convertSegmentName(api->getPredictedNextState(locID)));fflush(stdout);
+						criticalState[nextLocID] = api->getCurrentState(
+								nextLocID);
+						printf(
+								"current state is current state: loc: %d and loc: %d at position: %s\n",
+								locID, nextLocID,
+								Segment::convertSegmentName(
+										api->getPredictedNextState(locID)));
+						fflush(stdout);
 					}
 				}
 			}
@@ -123,45 +168,62 @@ void handlePredictions(BidibApi *api){
 	}
 }
 
-bool startSelfTest(BidibApi *api){
+bool startSelfTest(BidibApi *api) {
 
-	if(api->selfTestTrack()){
+	if (api->selfTestTrack()) {
 		printf("selftest OK\n");
 		return true;
-	}else{
+	} else {
 		printf("ERROR while selftest.\n");
 		return false;
 	}
 }
 
-void switchRandomTurn(BidibApi *api){
+void switchRandomTurn(BidibApi *api) {
 	bool turnOk = true;
 
 	Turnout::turnoutID turnID;
-	do{
+	do {
 		turnOk = true;
-		turnID = (Turnout::turnoutID)(rand() % 7);
+		turnID = (Turnout::turnoutID) (rand() % 7);
 		Segment::segmentID segID;
-		switch(turnID){
-		case Turnout::TA1:		segID = Segment::TA1; break;
-		case Turnout::TA2:		segID = Segment::TA2; break;
-		case Turnout::TC:		segID = Segment::TC; break;
-		case Turnout::TNO:		segID = Segment::TNO; break;
-		case Turnout::TNW:		segID = Segment::TNW; break;
-		case Turnout::TSO:		segID = Segment::TSO; break;
-		case Turnout::TSW:		segID = Segment::TSW; break;
+		switch (turnID) {
+		case Turnout::TA1:
+			segID = Segment::TA1;
+			break;
+		case Turnout::TA2:
+			segID = Segment::TA2;
+			break;
+		case Turnout::TC:
+			segID = Segment::TC;
+			break;
+		case Turnout::TNO:
+			segID = Segment::TNO;
+			break;
+		case Turnout::TNW:
+			segID = Segment::TNW;
+			break;
+		case Turnout::TSO:
+			segID = Segment::TSO;
+			break;
+		case Turnout::TSW:
+			segID = Segment::TSW;
+			break;
 		}
-		for(int locID = 0; locID < locCount; locID++){
-			if((api->isLocOnPosition(locID, segID)) || (api->getPredictedNextState(locID)) || (api->getCurrentState(locID))){
+		for (int locID = 0; locID < locCount; locID++) {
+			if ((api->isLocOnPosition(locID, segID))
+					|| (api->getPredictedNextState(locID))
+					|| (api->getCurrentState(locID))) {
 				turnOk = false;
 			}
 		}
-	}while(!turnOk);
+	} while (!turnOk);
 	api->toggleTurnoutState(turnID);
 }
 
 int main() {
-	printf("Program Start...\n\n"); fflush(stdout);
+	printf("Program Start...\n\n");
+	fflush(stdout);
 	BidibApi api;
 
 	api.setAllTurnoutsState(Turnout::straightOn);
@@ -169,15 +231,15 @@ int main() {
 	api.setTurnoutState(Turnout::TNO, Turnout::bendOff);
 //	api.setTurnoutState(Turnout::TA1, Turnout::bendOff);
 
-	while(api.isConnected()){
+	while (api.isConnected()) {
 		api.selfTestTurnout();
 
 		handlePredictions(&api);
 	}
 
-	printf("\nProgram End... \n"); fflush(stdout);
+	printf("\nProgram End... \n");
+	fflush(stdout);
 
 	return 0;
 }
-
 
--- src/BidibApi.cpp
+++ src/BidibApi.cpp
@@ -9,11 +9,11 @@
 #include "BidibApi.h"
 
 BidibApi::BidibApi() {
-	while(bidibMessageHandler.isConnected() != 1){
-		if(bidibMessageHandler.isConnected() == -1){
+	while (bidibMessageHandler.isConnected() != 1) {
+		if (bidibMessageHandler.isConnected() == -1) {
 			return;
 		}
-		usleep(1000*1000);
+		usleep(1000 * 1000);
 	}
 }
 
@@ -33,39 +33,39 @@ bool BidibApi::selfTestTurnout() {
 	setAllTurnoutsState(Turnout::straightOn);
 
 	setTurnoutState(Turnout::TNW, Turnout::bendOff);
-	usleep(1000*1000);
+	usleep(1000 * 1000);
 	setTurnoutState(Turnout::TNW, Turnout::straightOn);
-	usleep(1000*1000);
+	usleep(1000 * 1000);
 
 	setTurnoutState(Turnout::TA1, Turnout::bendOff);
-	usleep(1000*1000);
+	usleep(1000 * 1000);
 	setTurnoutState(Turnout::TA1, Turnout::straightOn);
-	usleep(1000*1000);
+	usleep(1000 * 1000);
 
 	setTurnoutState(Turnout::TNO, Turnout::bendOff);
-	usleep(1000*1000);
+	usleep(1000 * 1000);
 	setTurnoutState(Turnout::TNO, Turnout::straightOn);
-	usleep(1000*1000);
+	usleep(1000 * 1000);
 
 	setTurnoutState(Turnout::TSO, Turnout::bendOff);
-	usleep(1000*1000);
+	usleep(1000 * 1000);
 	setTurnoutState(Turnout::TSO, Turnout::straightOn);
-	usleep(1000*1000);
+	usleep(1000 * 1000);
 
 	setTurnoutState(Turnout::TA2, Turnout::bendOff);
-	usleep(1000*1000);
+	usleep(1000 * 1000);
 	setTurnoutState(Turnout::TA2, Turnout::straightOn);
-	usleep(1000*1000);
+	usleep(1000 * 1000);
 
 	setTurnoutState(Turnout::TSW, Turnout::bendOff);
-	usleep(1000*1000);
+	usleep(1000 * 1000);
 	setTurnoutState(Turnout::TSW, Turnout::straightOn);
-	usleep(1000*1000);
+	usleep(1000 * 1000);
 
 	setTurnoutState(Turnout::TC, Turnout::bendOff);
-	usleep(1000*1000);
+	usleep(1000 * 1000);
 	setTurnoutState(Turnout::TC, Turnout::straightOn);
-	usleep(1000*1000);
+	usleep(1000 * 1000);
 
 	printf("All Turnouts should be straight!\n");
 
@@ -75,132 +75,135 @@ bool BidibApi::selfTestTurnout() {
 bool BidibApi::selfTestTrack() {
 
 	std::vector<Segment::segmentID> sequence;
-		sequence.push_back(Segment::A2);
-		sequence.push_back(Segment::TA2);
-		sequence.push_back(Segment::TSW);
-		sequence.push_back(Segment::W3);
-		sequence.push_back(Segment::W2);
-		sequence.push_back(Segment::W1);
-		sequence.push_back(Segment::TNW);
-		sequence.push_back(Segment::N1);
-		sequence.push_back(Segment::N2);
-		sequence.push_back(Segment::N3);
-		sequence.push_back(Segment::TNO);
-		sequence.push_back(Segment::O1);
-		sequence.push_back(Segment::O2);
-		sequence.push_back(Segment::O3);
-		sequence.push_back(Segment::TSO);
-		sequence.push_back(Segment::S3);
-		sequence.push_back(Segment::S2);
-		sequence.push_back(Segment::S1);
-		sequence.push_back(Segment::TSW);
-		sequence.push_back(Segment::W3);
-		sequence.push_back(Segment::W2);
-		sequence.push_back(Segment::W1);
-		sequence.push_back(Segment::TNW);
-		sequence.push_back(Segment::NW1);
-		sequence.push_back(Segment::NW2);
-		sequence.push_back(Segment::NW3);
-		sequence.push_back(Segment::TC);
-		sequence.push_back(Segment::SO1);
-		sequence.push_back(Segment::SO2);
-		sequence.push_back(Segment::SO3);
-		sequence.push_back(Segment::TSO);
-		sequence.push_back(Segment::O3);
-		sequence.push_back(Segment::O2);
-		sequence.push_back(Segment::O1);
-		sequence.push_back(Segment::TNO);
-		sequence.push_back(Segment::TA1);
-		sequence.push_back(Segment::NO1);
-		sequence.push_back(Segment::NO2);
-		sequence.push_back(Segment::TC);
-		sequence.push_back(Segment::SW1);
-		sequence.push_back(Segment::SW2);
-		sequence.push_back(Segment::TA2);
-		sequence.push_back(Segment::TSW);
-		sequence.push_back(Segment::W3);
-		sequence.push_back(Segment::W2);
-		sequence.push_back(Segment::W1);
-		sequence.push_back(Segment::TNW);
-		sequence.push_back(Segment::NW1);
-		sequence.push_back(Segment::NW2);
-		sequence.push_back(Segment::NW3);
-		sequence.push_back(Segment::TC);
-		sequence.push_back(Segment::SO1);
-		sequence.push_back(Segment::SO2);
-		sequence.push_back(Segment::SO3);
-		sequence.push_back(Segment::TSO);
-		sequence.push_back(Segment::O3);
-		sequence.push_back(Segment::O2);
-		sequence.push_back(Segment::O1);
-		sequence.push_back(Segment::TNO);
-		sequence.push_back(Segment::TA1);
-		sequence.push_back(Segment::A1);
+	sequence.push_back(Segment::A2);
+	sequence.push_back(Segment::TA2);
+	sequence.push_back(Segment::TSW);
+	sequence.push_back(Segment::W3);
+	sequence.push_back(Segment::W2);
+	sequence.push_back(Segment::W1);
+	sequence.push_back(Segment::TNW);
+	sequence.push_back(Segment::N1);
+	sequence.push_back(Segment::N2);
+	sequence.push_back(Segment::N3);
+	sequence.push_back(Segment::TNO);
+	sequence.push_back(Segment::O1);
+	sequence.push_back(Segment::O2);
+	sequence.push_back(Segment::O3);
+	sequence.push_back(Segment::TSO);
+	sequence.push_back(Segment::S3);
+	sequence.push_back(Segment::S2);
+	sequence.push_back(Segment::S1);
+	sequence.push_back(Segment::TSW);
+	sequence.push_back(Segment::W3);
+	sequence.push_back(Segment::W2);
+	sequence.push_back(Segment::W1);
+	sequence.push_back(Segment::TNW);
+	sequence.push_back(Segment::NW1);
+	sequence.push_back(Segment::NW2);
+	sequence.push_back(Segment::NW3);
+	sequence.push_back(Segment::TC);
+	sequence.push_back(Segment::SO1);
+	sequence.push_back(Segment::SO2);
+	sequence.push_back(Segment::SO3);
+	sequence.push_back(Segment::TSO);
+	sequence.push_back(Segment::O3);
+	sequence.push_back(Segment::O2);
+	sequence.push_back(Segment::O1);
+	sequence.push_back(Segment::TNO);
+	sequence.push_back(Segment::TA1);
+	sequence.push_back(Segment::NO1);
+	sequence.push_back(Segment::NO2);
+	sequence.push_back(Segment::TC);
+	sequence.push_back(Segment::SW1);
+	sequence.push_back(Segment::SW2);
+	sequence.push_back(Segment::TA2);
+	sequence.push_back(Segment::TSW);
+	sequence.push_back(Segment::W3);
+	sequence.push_back(Segment::W2);
+	sequence.push_back(Segment::W1);
+	sequence.push_back(Segment::TNW);
+	sequence.push_back(Segment::NW1);
+	sequence.push_back(Segment::NW2);
+	sequence.push_back(Segment::NW3);
+	sequence.push_back(Segment::TC);
+	sequence.push_back(Segment::SO1);
+	sequence.push_back(Segment::SO2);
+	sequence.push_back(Segment::SO3);
+	sequence.push_back(Segment::TSO);
+	sequence.push_back(Segment::O3);
+	sequence.push_back(Segment::O2);
+	sequence.push_back(Segment::O1);
+	sequence.push_back(Segment::TNO);
+	sequence.push_back(Segment::TA1);
+	sequence.push_back(Segment::A1);
 
 	setAllTurnoutsState(Turnout::straightOn);
-	printf("You can place the loc now on segment A2 in direction TA2 and start ... \n");
-	while(getNumberOfLocs() == 0){
-		usleep(1000*1000);
+	printf(
+			"You can place the loc now on segment A2 in direction TA2 and start ... \n");
+	while (getNumberOfLocs() == 0) {
+		usleep(1000 * 1000);
 	}
 
-	if(getNumberOfLocs() > 1){
-		printf("To much locs on track, please place only ONE loc on segment A2 in direction TA2 and start again.\n");
+	if (getNumberOfLocs() > 1) {
+		printf(
+				"To much locs on track, please place only ONE loc on segment A2 in direction TA2 and start again.\n");
 		return false;
 	}
 
 	int locID = getNumberOfLocs() - 1;
-	int locSpeed = 100 ;
+	int locSpeed = 100;
 
 	int sequenceIterator = 0;
 	int o2Iterator = 0;
 
 	Segment::segmentID nextPosition = sequence.at(sequenceIterator++);
 
-	while(!isLocOnPosition(locID , nextPosition)){
-		usleep(1000*1000);
+	while (!isLocOnPosition(locID, nextPosition)) {
+		usleep(1000 * 1000);
 	}
 
 	Segment::segmentID currentPosition = nextPosition;
 	nextPosition = sequence.at(sequenceIterator++);
 
-	for(int i = 10; i > 0 ; i--){
-		printf("Loc starts in %d \n" , i);
+	for (int i = 10; i > 0; i--) {
+		printf("Loc starts in %d \n", i);
 		usleep(1000 * 1000);
 	}
 	printf("Loc starts!\n");
 
 	setLocSpeed(locID, locSpeed, true);
 
-	std::vector<Segment::segmentID> positions (MAXNUMBEROFSEGEMENTSWITHLOC);
+	std::vector<Segment::segmentID> positions(MAXNUMBEROFSEGEMENTSWITHLOC);
 	positions = getLocPosition(locID);
 
-	while(!isLocOnPosition(locID, sequence.back())){
+	while (!isLocOnPosition(locID, sequence.back())) {
 
-		if(positions == getLocPosition(locID)){
+		if (positions == getLocPosition(locID)) {
 			usleep(1000);
-		}else{
+		} else {
 			positions = getLocPosition(locID);
-			if(isLocOnPosition(locID, nextPosition)){
+			if (isLocOnPosition(locID, nextPosition)) {
 				currentPosition = nextPosition;
 				nextPosition = sequence.at(sequenceIterator++);
 
-				if(currentPosition == Segment::S1){
+				if (currentPosition == Segment::S1) {
 					setTurnoutState(Turnout::TNW, Turnout::bendOff);
 				}
 
-				if(currentPosition == Segment::SO3){
+				if (currentPosition == Segment::SO3) {
 					setTurnoutState(Turnout::TNO, Turnout::bendOff);
 					setTurnoutState(Turnout::TA1, Turnout::bendOff);
 				}
 
-				if(currentPosition == Segment::O2){
-					if(++o2Iterator == 3){
+				if (currentPosition == Segment::O2) {
+					if (++o2Iterator == 3) {
 						setTurnoutState(Turnout::TA1, Turnout::straightOn);
 					}
 				}
-			}else{
-				if(!isLocOnPosition(locID, currentPosition)){
-					printf("ERROR segment Number: %d not found\n", nextPosition);
+			} else {
+				if (!isLocOnPosition(locID, currentPosition)) {
+					printf("ERROR segment Number: %d not found\n",
+							nextPosition);
 					setLocSpeed(locID, 1, true);
 					return false;
 				}
@@ -209,7 +212,6 @@ bool BidibApi::selfTestTrack() {
 	}
 
 	setLocSpeed(locID, 1, true);
-
 
 	return true;
 }
@@ -223,7 +225,7 @@ uint32_t BidibApi::getAllLocPosition() {
 }
 
 void BidibApi::setLocSpeed(int locID, int speed, bool forward) {
-	bidibMessageHandler.sendDriveMessage(locID, speed , forward);
+	bidibMessageHandler.sendDriveMessage(locID, speed, forward);
 }
 
 int BidibApi::getLocSpeed(int locID) {
@@ -238,14 +240,15 @@ bool BidibApi::getLocLogicalDirection(int locID) {
 	return bidibMessageHandler.locs[locID].stateMaschine.logicalDirection;
 }
 
-void BidibApi::setLocFunction(int locID, locFunction::locFunctionID ID, bool functionState) {
+void BidibApi::setLocFunction(int locID, locFunction::locFunctionID ID,
+		bool functionState) {
 	bidibMessageHandler.sendFunctionStateMessage(locID, ID, functionState);
 }
 
 bool BidibApi::getLocFunctionstate(int locID, locFunction::locFunctionID ID) {
-	if(ID == locFunction::FLight){
+	if (ID == locFunction::FLight) {
 		return bidibMessageHandler.locs[locID].lightState;
-	}else{
+	} else {
 		return bidibMessageHandler.locs[locID].functionState[ID];
 	}
 }
@@ -255,9 +258,9 @@ Turnout::turnDirection BidibApi::getTurnoutState(Turnout::turnoutID turnID) {
 }
 
 std::vector<Turnout> BidibApi::getAllTurnoutStates() {
-	std::vector<Turnout> turnouts (MAXNUMBEROFTURNOUTS);
+	std::vector<Turnout> turnouts(MAXNUMBEROFTURNOUTS);
 
-	for(int i = 0; i < MAXNUMBEROFTURNOUTS; i++){
+	for (int i = 0; i < MAXNUMBEROFTURNOUTS; i++) {
 		turnouts.push_back(bidibMessageHandler.turnouts[i]);
 	}
 
@@ -265,18 +268,18 @@ std::vector<Turnout> BidibApi::getAllTurnoutStates() {
 }
 
 std::vector<Segment::segmentID> BidibApi::getLocPosition(int locID) {
-	std::vector<Segment::segmentID> segID (MAXNUMBEROFSEGEMENTSWITHLOC);
+	std::vector<Segment::segmentID> segID(MAXNUMBEROFSEGEMENTSWITHLOC);
 
-	for(int i = 0; i < MAXNUMBEROFSEGEMENTSWITHLOC; i++){
+	for (int i = 0; i < MAXNUMBEROFSEGEMENTSWITHLOC; i++) {
 		segID[i] = Segment::DEFAULT;
 	}
 
 	int index = 0;
 	uint32_t position = bidibMessageHandler.locs[locID].position;
 
-	if(locID < getNumberOfLocs()){
-		for(int i = 0; i < MAXNUMBEROFSEGMENTS; i++){
-			if(position & (1 << i)){
+	if (locID < getNumberOfLocs()) {
+		for (int i = 0; i < MAXNUMBEROFSEGMENTS; i++) {
+			if (position & (1 << i)) {
 				segID[index] = (Segment::segmentID) i;
 				index++;
 			}
@@ -285,35 +288,36 @@ std::vector<Segment::segmentID> BidibApi::getLocPosition(int locID) {
 	return segID;
 }
 
-void BidibApi::setTurnoutState(Turnout::turnoutID turnID, Turnout::turnDirection turnDir) {
-	if(MAKROTURN){
+void BidibApi::setTurnoutState(Turnout::turnoutID turnID,
+		Turnout::turnDirection turnDir) {
+	if (MAKROTURN) {
 		bidibMessageHandler.sendMakro(turnID, turnDir);
-	}else{
+	} else {
 		bidibMessageHandler.sendTurnMessage(turnID, turnDir);
 	}
 }
 
 void BidibApi::setAllTurnoutsState(Turnout::turnDirection turnDir) {
-	for(int i = 0; i < MAXNUMBEROFTURNOUTS; i++){
-		setTurnoutState((Turnout::turnoutID) i , turnDir);
+	for (int i = 0; i < MAXNUMBEROFTURNOUTS; i++) {
+		setTurnoutState((Turnout::turnoutID) i, turnDir);
 	}
 }
 
 bool BidibApi::isConnected() {
-	if((bidibMessageHandler.isConnected() == 0) || (bidibMessageHandler.isConnected() == -1)){
+	if ((bidibMessageHandler.isConnected() == 0)
+			|| (bidibMessageHandler.isConnected() == -1)) {
 		return false;
 	}
-	if(bidibMessageHandler.isConnected() == 1){
+	if (bidibMessageHandler.isConnected() == 1) {
 		return true;
 	}
-
 }
 
 bool BidibApi::isLocOnPosition(int locID, Segment::segmentID segID) {
-	std::vector<Segment::segmentID> segments (MAXNUMBEROFSEGEMENTSWITHLOC);
+	std::vector<Segment::segmentID> segments(MAXNUMBEROFSEGEMENTSWITHLOC);
 	segments = getLocPosition(locID);
-	for(int i = 0; i < MAXNUMBEROFSEGEMENTSWITHLOC; i++){
-		if(segments[i] == segID){
+	for (int i = 0; i < MAXNUMBEROFSEGEMENTSWITHLOC; i++) {
+		if (segments[i] == segID) {
 			return true;
 		}
 	}
@@ -322,8 +326,8 @@ bool BidibApi::isLocOnPosition(int locID, Segment::segmentID segID) {
 
 int BidibApi::getLocsOnSegment(Segment::segmentID segID) {
 	int locsOnSegment = 0;
-	for (int i=0; i<bidibMessageHandler.locCount; i++){
-		if(bidibMessageHandler.locs[i].position == segID){
+	for (int i = 0; i < bidibMessageHandler.locCount; i++) {
+		if (bidibMessageHandler.locs[i].position == segID) {
 			locsOnSegment |= (1 << i);
 		}
 	}
@@ -343,9 +347,9 @@ Segment::segmentID BidibApi::getPreviousState(int locID) {
 }
 
 void BidibApi::toggleTurnoutState(Turnout::turnoutID turnID) {
-	if(getTurnoutState(turnID) == Turnout::straightOn){
+	if (getTurnoutState(turnID) == Turnout::straightOn) {
 		setTurnoutState(turnID, Turnout::bendOff);
-	}else{
+	} else {
 		setTurnoutState(turnID, Turnout::straightOn);
 	}
 }
